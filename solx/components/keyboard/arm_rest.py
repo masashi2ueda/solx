e3603ff1020478577da34702d9ed49b71c6bbaa7
masashi ueda <ronio2011@yahoo.co.jp>
2026-01-23 10:27:08 +0900

    arm_restまでok

--- solx/components/keyboard/arm_rest.py
+++ solx/components/keyboard/arm_rest.py
@@ -4,125 +4,10 @@ from __future__ import annotations
 import copy
 from typing import TypeVar
 
-from solx.core import SolxObject, Vec3
+from solx.core import SolxObject, Vec3, param_apply
 from solx.primitives import Cube, Cylinder, PolygonExtrude
 
 T = TypeVar("T", bound=SolxObject)
-
-
-# %%
-
-c_w = 10.0
-c_d = 5.0
-c_h = 5.0
-c_dx = 5.0
-
-plate_w = 110.0
-plate_d = 130.0
-
-plat_radius = 50.0
-plate_h = 2.0
-
-# plate_w = plate_w - plat_radius * 2
-# plate_d = plate_d - plat_radius * 2
-
-plate_w2 = plate_w * 0.9
-plate_d_m = plate_d - plate_w2 / 2
-
-
-
-plate = PolygonExtrude(
-    points=[
-        (plate_w / 2, 0),
-        (plate_w2 / 2, plate_d_m),
-        (0, plate_d),
-        (-plate_w2 / 2, plate_d_m),
-        (-plate_w / 2, 0),
-    ],
-    height=plate_h,
-)
-cyl = Cylinder(radius=plate_w2 / 2, height=plate_h, segments=64)
-cyl = cyl.translate((0, plate_d_m, 0))
-dst = plate + cyl
-
-c_cube = Cube(size=(c_w, c_d, c_h)).translate((0, c_d / 2, plate_h))
-c_cube_tx = -c_w / 2 + plate_w / 2 - c_dx
-c_cube = c_cube.translate((c_cube_tx, 0, 0))
-dst += c_cube
-
-dst.render()
-
-# %%
-class InsetBearing(SolxObject):
-    def __init__(
-        self,
-        c_c_cube_w1 = 3.0,
-        c_c_cube_w2 = 5.0,
-        c_c_cube_d2 = 2.0,
-        c_c_r1 = 7,
-        c_c_r2 = 3,
-        c_c_h = 10.0,
-        c_mgn = 0.3,
-    ):
-        cyl1 = Cylinder(radius=c_c_r1, height=c_c_h, segments=64)
-        cyl2 = Cylinder(radius=c_c_r2, height=c_c_h, segments=64)
-        cyl = cyl1 - cyl2
-
-        c1_w = c_c_cube_w1
-        c1_d = c_c_r1
-        c1_h = c_c_h
-        c1 = Cube(size=(c1_w, c1_d, c1_h))
-        c1_ty = -c1_d / 2
-        c1 = c1.translate((0, c1_ty, 0))
-        cyl -= c1
-
-        c2_w = c_c_cube_w2
-        c2_d = c_c_cube_d2
-        c2_h = c_c_h
-        c2 = Cube(size=(c2_w, c2_d, c2_h))
-        c2_ty = - c_c_r2 - (c_c_r1 - c_c_r2) / 2
-        c2 = c2.translate((0, c2_ty, 0))
-        cyl -= c2
-
-        inset1 = Cube(size=(c1_w - c_mgn, c1_d  - c_mgn , c1_h))
-        inset1 = inset1.translate((0, c1_ty, 0))
-        inset2 = Cube(size=(c2_w - c_mgn, c2_d - c_mgn, c2_h))
-        inset2 = inset2.translate((0, c2_ty, 0))
-        inset = inset1 + inset2 - cyl2
-    
-        super().__init__(openscad_node=cyl.node)
-        self.inset = inset
-
-    def param_apply(self, func_name: str, params: Vec3) -> InsetBearing:
-        dst = copy.deepcopy(self)
-        dst = dst.param_apply(func_name, params)
-        dst.inset = self.inset.param_apply(func_name, params)
-        return dst
-
-
-c_c_cube_w1 = 3.0
-c_c_cube_w2 = 5.0
-c_c_cube_d2 = 2.0
-
-c_c_r1 = 7
-c_c_r2 = 3
-c_c_h = 10.0
-
-c_mgn = 0.3
-
-inset_bearing = InsetBearing(
-    c_c_cube_w1=c_c_cube_w1,
-    c_c_cube_w2=c_c_cube_w2,
-    c_c_cube_d2=c_c_cube_d2,
-    c_c_r1=c_c_r1,
-    c_c_r2=c_c_r2,
-    c_c_h=c_c_h,
-    c_mgn=c_mgn,
-)
-
-(inset_bearing + inset_bearing.inset).render()
-# inset.render()
-
 
 # %%
 class SlottedBearing(SolxObject):
@@ -176,22 +61,136 @@ class SlottedBearing(SolxObject):
 
     def param_apply(self, func_name: str, params: Vec3) -> SlottedBearing:
         dst = copy.deepcopy(self)
-        dst = dst.param_apply(func_name, params)
+        dst.node = param_apply(func_name, params, self.node)
         dst.inset = self.inset.param_apply(func_name, params)
         return dst
 
 
-slotted_bearing = SlottedBearing(
-    outer_radius=7.0,
-    inner_radius=3.0,
-    height=10.0,
-    slot_width_narrow=3.0,
-    slot_width_wide=5.0,
-    slot_depth_wide=2.0,
-    clearance_margin=0.3,
-)
+class ArmRestPlate(SolxObject):
+    def __init__(
+        self,
+        plate_w = 110.0,
+        plate_d = 130.0,
+        plate_h = 2.0,
+        plate_w2_ratio = 0.9,
+    ):
+        plate_w2 = plate_w * plate_w2_ratio
+        plate_d_m = plate_d - plate_w2 / 2
 
+        plate = PolygonExtrude(
+            points=[
+                (plate_w / 2, 0),
+                (plate_w2 / 2, plate_d_m),
+                (0, plate_d),
+                (-plate_w2 / 2, plate_d_m),
+                (-plate_w / 2, 0),
+            ],
+            height=plate_h,
+        )
+        cyl = Cylinder(radius=plate_w2 / 2, height=plate_h, segments=64)
+        cyl = cyl.translate((0, plate_d_m, 0))
+        dst = plate + cyl
+        super().__init__(openscad_node=dst.node)
+
+class ArmRest(SolxObject):
+    def __init__(
+        self,
+        # plate
+        plate_w = 110.0,
+        plate_d = 130.0,
+        plate_h = 2.0,
+        plate_w2_ratio = 0.9,
+        # slot cube
+        cube_w = 10.0,
+        cube_d = 5.0,
+        cube_h = 5.0,
+        cube_dx = 5.0,
+
+        # slot ling
+        bearing_outer_radius=7.0,
+        bearing_inner_radius=3.0,
+        bearing_height=10.0,
+        bearing_slot_width_narrow=3.0,
+        bearing_slot_width_wide=5.0,
+        bearing_slot_depth_wide=2.0,
+        bearing_clearance_margin=0.3
+    ):
+        # plate
+        arm_rest_plate = ArmRestPlate(
+            plate_w=plate_w,
+            plate_d=plate_d,
+            plate_h=plate_h,
+            plate_w2_ratio=plate_w2_ratio,
+        )
+        dst = arm_rest_plate
+
+        c_cube = Cube(size=(cube_w, cube_d, cube_h))
+        cube_dy = cube_d / 2
+        cube_dz = plate_h
+        c_cube = c_cube.translate((0, cube_dy, cube_dz))
+        c_cube_tx_r = -cube_w / 2 + plate_w / 2 - cube_dx
+        c_cube_tx_l = -1 * c_cube_tx_r
+        dst += c_cube.translate((c_cube_tx_r, 0, 0))
+        dst += c_cube.translate((c_cube_tx_l, 0, 0))
+
+        slotted_bearing = SlottedBearing(
+            outer_radius=bearing_outer_radius,
+            inner_radius=bearing_inner_radius,
+            height=bearing_height,
+            slot_width_narrow=bearing_slot_width_narrow,
+            slot_width_wide=bearing_slot_width_wide,
+            slot_depth_wide=bearing_slot_depth_wide,
+            clearance_margin=bearing_clearance_margin,
+        )
+        slotted_bearing = slotted_bearing.rotate((90, 0, 0))
+        slotted_bearing = slotted_bearing.rotate((0, 0, 90))
+        slotted_bearing = slotted_bearing.rotate((180, 0, 0))
+        slot_dz = cube_h + cube_dz + bearing_outer_radius - 2
+        slotted_bearing = slotted_bearing.translate((-bearing_height/2, cube_dy, slot_dz))
+        dst += slotted_bearing.translate((c_cube_tx_r, 0, 0))
+        dst += slotted_bearing.translate((c_cube_tx_l, 0, 0))
+        super().__init__(openscad_node=dst.node)
+
+        self.slotted_bearing = slotted_bearing
+# %%
 if __name__ == "__main__":
-    (slotted_bearing + slotted_bearing.inset).render()
+    # plate
+    plate_w = 110.0
+    plate_d = 130.0
+    plate_h = 2.0
+    plate_w2_ratio = 0.9
 
+    # slot cube
+    cube_w = 10.0
+    cube_d = 5.0
+    cube_h = 5.0
+    cube_dx = 5.0
+
+    # slot ling
+    bearing_outer_radius=7.0
+    bearing_inner_radius=3.0
+    bearing_height=10.0
+    bearing_slot_width_narrow=3.0
+    bearing_slot_width_wide=5.0
+    bearing_slot_depth_wide=2.0
+    bearing_clearance_margin=0.3
+
+    arm_rest = ArmRest(
+        plate_w=plate_w,
+        plate_d=plate_d,
+        plate_h=plate_h,
+        plate_w2_ratio=plate_w2_ratio,
+        cube_w=cube_w,
+        cube_d=cube_d,
+        cube_h=cube_h,
+        cube_dx=cube_dx,
+        bearing_outer_radius=bearing_outer_radius,
+        bearing_inner_radius=bearing_inner_radius,
+        bearing_height=bearing_height,
+        bearing_slot_width_narrow=bearing_slot_width_narrow,
+        bearing_slot_width_wide=bearing_slot_width_wide,
+        bearing_slot_depth_wide=bearing_slot_depth_wide,
+        bearing_clearance_margin=bearing_clearance_margin
+    )
+    arm_rest.render()
 # %%
