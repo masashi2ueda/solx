1527f575202132da620a12e606b189e84f65c346
masashi ueda <ronio2011@yahoo.co.jp>
2026-04-15 20:51:47 +0900

    Refactor keycap creation logic and enhance STL export functionality

--- solx/components/keyboard/keycap.py
+++ solx/components/keyboard/keycap.py
@@ -6,8 +6,7 @@ specifically designed for Choc V2 switches. It combines keycap tops with stems t
 complete keycap assemblies.
 """
 # %%
-
-from functools import partial
+import numpy as np
 
 from solx.components.keyboard.keycap_top import create_keycap_top
 from solx.components.keyboard.stem_chocv2 import ChocV2Stem
@@ -16,118 +15,122 @@ from solx.primitives.cylinder import Cylinder
 
 
 def create_keycap_with_stem(
-    top_offset_x = 0.0,
-    top_offset_y = 0.0,
-    top_offset_z = 1.5,
-    bottom_w = 16.0,
-    bottom_d = 16.0,
-    top_w = 16.0,
-    top_d = 16.0,
-    height = 5.0,
-    bottom_radius = 3,
-    top_radius = 3,
-    top_dx = 0.0,
-    top_dy = 0.0,
-    top_rx_deg = 0.0,
-    top_ry_deg = 0.0,
-    top_rz_deg = 0.0,
-    dimple = 0.0,
-    # home_bump = False,
-    # home_bump_radius = 0.8,
-    # home_bump_height = 0.4,
-    # home_bump_length = 2.5,
-    # home_bump_offset_x = 0.0,
-    # home_bump_offset_y = 0.0,
-    # home_bump_offset_y_ratio = -0.8,
-    segments = 64,
-    # offset_wd = 1,
-    # offset_h_ratio = 0.5,
-    # stem_height=6.5,
+    keycap_top,
+    top_offset_x,
+    top_offset_y,
+    top_offset_z,
     inverted = False,
     stem_radius = 2.765,
 ) -> SolxObject:
-    """Create a keycap with a Choc V2 stem attached.
-
-    Args:
-        top_offset_x (float): X offset of the keycap top from the stem.
-        top_offset_y (float): Y offset of the keycap top from the stem.
-        top_offset_z (float): Z offset of the keycap top from the stem.
-        bottom_w (float): Width of the bottom face of the keycap top.
-        bottom_d (float): Depth of the bottom face of the keycap top.
-        top_w (float): Width of the top face of the keycap top.
-        top_d (float): Depth of the top face of the keycap top.
-        height (float): Height of the keycap top.
-        bottom_radius (float): Radius of the bottom face corners of the keycap top.
-        top_radius (float): Radius of the top face corners of the keycap top.
-        top_dx (float): X offset of the top face of the keycap top.
-        top_dy (float): Y offset of the top face of the keycap top.
-        top_rx_deg (float): Rotation around X axis of the top face in degrees.
-        top_ry_deg (float): Rotation around Y axis of the top face in degrees.
-        top_rz_deg (float): Rotation around Z axis of the top face in degrees.
-        dimple (float): Depth of the dimple on the top face of the keycap top.
-        home_bump (bool): If True, add a small bump for home position.
-        home_bump_radius (float): Radius of the home position bump.
-        home_bump_height (float): Height of the home position bump.
-        home_bump_length (float): Length of the home position bump.
-        home_bump_offset_x (float): X offset of the bump on the top face.
-        home_bump_offset_y (float): Y offset of the bump on the top face.
-        home_bump_offset_y_ratio (float|None): If set, overrides Y offset using a ratio
-            of half the top depth (-1.0: front edge, 0: center, 1.0: back edge).
-        segments (int): Number of segments for rounded corners of the keycap top.
-        offset_wd (float): Wall thickness of the keycap top.
-        offset_h_ratio (float): Ratio of height to offset the inner hollow part.
-        inverted (bool): If True, creates an inverted keycap (upside down).
-
-    Returns:
-        SolxObject: The keycap with Choc V2 stem attached.
-    """
-    create_key_top_h = partial(create_keycap_top,
-        bottom_w = bottom_w,
-        bottom_d = bottom_d,
-        top_w = top_w,
-        top_d = top_d,
-        bottom_radius = bottom_radius,
-        top_radius = top_radius,
-        top_dx = top_dx,
-        top_dy = top_dy,
-        top_rx_deg = top_rx_deg,
-        top_ry_deg = top_ry_deg,
-        top_rz_deg = top_rz_deg,
-        dimple = dimple,
-        # home_bump = home_bump,
-        # home_bump_radius = home_bump_radius,
-        # home_bump_height = home_bump_height,
-        # home_bump_length = home_bump_length,
-        # home_bump_offset_x = home_bump_offset_x,
-        # home_bump_offset_y = home_bump_offset_y,
-        # home_bump_offset_y_ratio = home_bump_offset_y_ratio,
-        segments = segments,
-        # offset_wd = offset_wd,
-        # offset_h_ratio = offset_h_ratioa
-        )
-    keycap_top = create_key_top_h(height=height)
-    # keycat_top2 = create_key_top_h(height=200, top_rx_deg=0, top_ry_deg=0, top_rz_deg=0)
-    # keycat_top3 = create_key_top_h(height=200, top_rx_deg=0, top_ry_deg=0)
-
     stem_core = ChocV2Stem(stem_radius=stem_radius)
     stem_cylinder = Cylinder(radius=stem_core.stem_radius,height=top_offset_z)
-
     keycap_top = keycap_top.translate((top_offset_x, top_offset_y, top_offset_z))
-    # keycat_top2 = keycat_top2.translate((top_offset_x, top_offset_y, top_offset_z))
-    # keycat_top3 = keycat_top3.translate((top_offset_x, top_offset_y, top_offset_z))
-
-    # stem_cylinder -= keycat_top2
-    # stem_cylinder -= keycat_top3
     keycap = keycap_top + stem_cylinder
     keycap -= stem_core.cross_slot
     if inverted:
         keycap = keycap.rotate_x(180)
     return keycap
+# %%
+LR = 'R'
+
+K_MGN_Y = 17.0
+BASE_KEY_SIZE = BASE_W = BASE_D = 15.5
+BOTTOM_W = BASE_KEY_SIZE
+BOTTOM_D = BASE_KEY_SIZE
+TOP_W = BASE_KEY_SIZE
+TOP_D = BASE_KEY_SIZE
+K_BLANK_Y = K_MGN_Y - BASE_KEY_SIZE
+
+def create_keycap(
+    d_keytop_size_d=0.0,
+    d_top_offset_y=0.0,
+    key_cap_dh=0.0,
+    top_rx_deg=0.0,
+):
+    d_keytop_size_d = d_keytop_size_d
+    d_top_offset_y = - d_top_offset_y
+    key_cap_top = create_keycap_top(
+        bottom_w=BOTTOM_W,
+        bottom_d=BOTTOM_D + d_keytop_size_d,
+        top_w=TOP_W,
+        top_d=TOP_D + d_keytop_size_d,
+        height=5.0 + key_cap_dh,
+        bottom_radius=3,
+        top_radius=3,
+        top_dx=0.0,
+        top_dy=0.0,
+        top_rx_deg=top_rx_deg,
+        top_ry_deg=0.0,
+        top_rz_deg=0.0,
+        is_dimple=True,
+        dimple_radius=40.0,
+        dimple_size=3.0,
+        dimple_segments=1000,
+        is_sohw_dimple=False,
+        segments=64,
+    )
+    dst = create_keycap_with_stem(
+        keycap_top=key_cap_top,
+        top_offset_x=0.0,
+        top_offset_y=d_top_offset_y,
+        top_offset_z=5.0,
+        inverted=False,
+        stem_radius=2.755,
+    )
+    return dst
+
+dy1 = 5.0
+
+d_keytop_d = - dy1 / 3
+hd1 = 5.0
+hd2 = 2.0
+rdx1 = np.atan2(hd1-hd2, TOP_D) * 180 / np.pi
+rdx2 = np.atan2(hd2, TOP_D) * 180 / np.pi + 5
+k1_org = create_keycap(
+    d_top_offset_y=dy1,
+    key_cap_dh=hd1,
+    top_rx_deg=rdx1,)
+k2_org = create_keycap(
+    d_top_offset_y=-2.5 * d_keytop_d,
+    d_keytop_size_d=d_keytop_d,
+    key_cap_dh=hd2,
+    top_rx_deg=rdx2,)
+k3_org = create_keycap(
+    d_top_offset_y=-1.5 * d_keytop_d,
+    d_keytop_size_d=d_keytop_d)
+k4_org = create_keycap(
+    d_top_offset_y=-0.5 * d_keytop_d,
+    d_keytop_size_d=d_keytop_d)
+
+def mytranslate(obj, x, y):
+    return obj.translate((x , -y, 0))
+k1 = mytranslate(k1_org, -23.0, -K_MGN_Y)
+k2 = mytranslate(k2_org, -23.0, 0.0)
+k3 = mytranslate(k3_org, -23.0, K_MGN_Y)
+k4 = mytranslate(k4_org, -23.0, 2 * K_MGN_Y)
+
+if LR == "L":
+    k1 = k1.mirror((1,0,0))
+    k2 = k2.mirror((1,0,0))
+    k3 = k3.mirror((1,0,0))
+    k4 = k4.mirror((1,0,0))
+
+dst = k1 + k2 + k3 + k4
+if True:
+    dst += dst.translate((17, 0, 0.0))
+dst.render()
+# %%
+from solx.config import EnvConfig
+
+# k1.save_stl(f"{EnvConfig.OUTPUT_STL_DIR_PATH}/{LR}_{rz}_k1_{int(stem_radius*1000)}.stl")
+k1.save_stl(f"{EnvConfig.OUTPUT_STL_DIR_PATH}/{LR}_k1.stl")
+k2.save_stl(f"{EnvConfig.OUTPUT_STL_DIR_PATH}/{LR}_k2.stl")
+k3.save_stl(f"{EnvConfig.OUTPUT_STL_DIR_PATH}/{LR}_k3.stl")
+k4.save_stl(f"{EnvConfig.OUTPUT_STL_DIR_PATH}/{LR}_k4.stl")
 
 
 
 # %%
-
 LR = "R"
 # LR = "L"
 # offset_x = 200
@@ -214,6 +217,8 @@ if LR == "L":
     k4 = k4.mirror((1,0,0))
 
 dst = k1 + k2 + k3 + k4
+if True:
+    dst += dst.translate((17, 0, 0.0))
 dst.render()
 # %%
 from solx.config import EnvConfig
