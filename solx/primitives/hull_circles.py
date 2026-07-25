b65515abbefe553e823e571de6fdd139570384a7
masashi ueda <ronio2011@yahoo.co.jp>
2026-07-25 22:56:04 +0900

    Refactor HullCircles constructor to accept circle_radiuses as a flexible type and update example usage in main block

--- solx/primitives/hull_circles.py
+++ solx/primitives/hull_circles.py
@@ -15,10 +15,13 @@ class HullCircles(SolxObject):
     def __init__(
         self,
         circle_positions: list[tuple[float, float]],
-        circle_radiuses: list[float],
+        circle_radiuses: list[float] | float | int,
         height: float,
     ):
         circles = []
+        if isinstance(circle_radiuses, float) or isinstance(circle_radiuses, int):
+            circle_radiuses = [circle_radiuses] * len(circle_positions)
+
         for (xy, r) in zip(circle_positions, circle_radiuses):
             x, y = xy
             circles.append(translate([x, y])(circle(r=r)))
@@ -43,11 +46,17 @@ class HullCircles(SolxObject):
 if __name__ == "__main__":
     height = 20
     xys = [(0, 0), (10, 10), (-10, -10), (20, -20), (-20, 20)]
-    rs = [1, 2, 3, 4, 5]
+    rs = [1, 1, 1, 1, 1]
 
     hull_circles = HullCircles(circle_positions=xys, circle_radiuses=rs, height=height)
     hull_circles = hull_circles.translate((10, 20, 30))
     print(hull_circles.circle_positions)
     hull_circles.render()
+
+    hull_circles = HullCircles(circle_positions=xys, circle_radiuses=5, height=height)
+    hull_circles = hull_circles.translate((10, 20, 30))
+    print(hull_circles.circle_positions)
+    hull_circles.render()
+
 
 # %%
